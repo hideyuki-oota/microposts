d8c2e3dec206d87677f05485d3bed914ad3f88a1
wivern <h-oota@mua.biglobe.ne.jp>
2016-07-19 10:50:52 +0000

    assignments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   
   def show 
     @user = User.find(params[:id])
@@ -17,12 +18,35 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      flash[:success] = "プロフィールを編集しました"
+      redirect_to root_path
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile)
   end
   
+  def correct_user
+
+    @user = User.find(params[:id])
+
+    if current_user != @user
+        redirect_to root_url
+    end
+  end
+
 end
