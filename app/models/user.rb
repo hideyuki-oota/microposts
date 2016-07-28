63fe922774e7c46be3ec63ae256bd5f9e1c1ff44
wivern <h-oota@mua.biglobe.ne.jp>
2016-07-28 10:42:57 +0000

    add followings followers list

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,8 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+  
+  has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  
 end
